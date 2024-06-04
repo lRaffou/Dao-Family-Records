f084d34702b1b5902d7a73bb4a8115f7539b22e9
lRaffou <raf2@hotmail.fr>
2024-06-04 23:21:20 +0200

    accueil v0.6

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+function send() {
+    window.open("mailto:r.lourenco@outlook.fr");
+}
+// const date = document.getElementsByClassName(date);
+// const moonLanding = new Date('July 20, 1969 00:20:18');
+// date.document.innerText(moonLanding.getFullYear());
+// date.write(new Date('June 20, 69 00:20:18').getFullYear())
+
+document.getElementById("year").innerHTML = new Date().getFullYear()
