b552e9851305ea8c15d2702a5b6752058cfa581f
lRaffou <raf2@hotmail.fr>
2024-06-19 18:07:51 +0200

    end of css carousel

--- JS/script.js
+++ JS/script.js
@@ -2,4 +2,31 @@ function send() {
     window.open("mailto:r.lourenco@outlook.fr");
 }
 
-document.getElementById("year").innerHTML = new Date().getFullYear();
+document.getElementById("year").innerText = new Date().getFullYear();
+
+// ____________________________________Carousel____________________________________
+
+const carousel = document.querySelector(".carousel");
+
+let isDragStart = false, prevPageX, prevScrollLeft;
+
+const dragStart = (e) => {
+    isDragStart = true;
+    prevPageX = e.pageX;
+    prevScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) => {
+    if(!isDragStart) return;
+    e.preventDefault();
+    let positionDiff = e.pageX - prevPageX;
+    carousel.scrollLeft = prevScrollLeft - positionDiff;
+}
+
+const dragStop = () => {
+    isDragStart = false;
+}
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("mouseup", dragStop);
