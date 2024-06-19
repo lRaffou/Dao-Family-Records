1c6dc12349a68eb9acdc3757583e58f2a6d07f49
lRaffou <raf2@hotmail.fr>
2024-06-19 20:04:51 +0200

    JS of carousel

--- JS/script.js
+++ JS/script.js
@@ -7,8 +7,17 @@ function send() {
 // ____________________________________Carousel____________________________________
 
 const carousel = document.querySelector(".carousel");
+const arrowIcons = document.querySelectorAll(".wrapper i");
+const firstImg = carousel.querySelectorAll("img")[0];
 
 let isDragStart = false, prevPageX, prevScrollLeft;
+let firstImgWidth = firstImg.clientWidth + 114;
+
+arrowIcons.forEach(icon => {
+    icon.addEventListener("click", () => {
+        carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+    })
+});
 
 const dragStart = (e) => {
     isDragStart = true;
@@ -19,14 +28,16 @@ const dragStart = (e) => {
 const dragging = (e) => {
     if(!isDragStart) return;
     e.preventDefault();
+    carousel.classList.add("dragging");
     let positionDiff = e.pageX - prevPageX;
     carousel.scrollLeft = prevScrollLeft - positionDiff;
 }
 
 const dragStop = () => {
     isDragStart = false;
+    carousel.classList.remove("dragging")
 }
 
-carousel.addEventListener("touchstart", dragStart);
-carousel.addEventListener("touchmove", dragging);
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
 carousel.addEventListener("mouseup", dragStop);
