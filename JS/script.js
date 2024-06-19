40b5cf5d40ef480c4fe289f5d6443231165b6940
lRaffou <raf2@hotmail.fr>
2024-06-19 22:53:59 +0200

    JS of carousel

--- JS/script.js
+++ JS/script.js
@@ -11,17 +11,25 @@ const arrowIcons = document.querySelectorAll(".wrapper i");
 const firstImg = carousel.querySelectorAll("img")[0];
 
 let isDragStart = false, prevPageX, prevScrollLeft;
-let firstImgWidth = firstImg.clientWidth + 114;
+let firstImgWidth = firstImg.clientWidth + 14;
+let scrollWidth = carousel.scrollWidth - carousel.clientWidth;
+
+const showHideIcons = () => {
+    arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
+    arrowIcons[1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
+}
 
 arrowIcons.forEach(icon => {
     icon.addEventListener("click", () => {
         carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+        setTimeout(() => showHideIcons(), 60);
+
     })
 });
 
 const dragStart = (e) => {
     isDragStart = true;
-    prevPageX = e.pageX;
+    prevPageX = e.pageX || e.touches[0].pageX;
     prevScrollLeft = carousel.scrollLeft;
 }
 
@@ -29,15 +37,21 @@ const dragging = (e) => {
     if(!isDragStart) return;
     e.preventDefault();
     carousel.classList.add("dragging");
-    let positionDiff = e.pageX - prevPageX;
+    let positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
     carousel.scrollLeft = prevScrollLeft - positionDiff;
+    showHideIcons();
 }
 
 const dragStop = () => {
     isDragStart = false;
-    carousel.classList.remove("dragging")
+    carousel.classList.remove("dragging");
 }
 
 carousel.addEventListener("mousedown", dragStart);
 carousel.addEventListener("mousemove", dragging);
 carousel.addEventListener("mouseup", dragStop);
+carousel.addEventListener("mouseleave", dragStop);
+
+carousel.addEventListener("touchestart", dragStart);
+carousel.addEventListener("touchmove", dragging);
+carousel.addEventListener("touchend", dragStop);
