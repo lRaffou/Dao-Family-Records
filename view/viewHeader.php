a9a8e59a565fe63ec73342ad6b94a56818fd1544
lRaffou <akira182@proton.me>
2024-10-09 19:24:34 +0200

    modif script.js on .php

--- view/viewHeader.php
+++ view/viewHeader.php
@@ -15,11 +15,9 @@ function renderHeader()
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
             integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
             crossorigin="anonymous" referrerpolicy="no-referrer" />
-        <?php
-        header("Content-type: /js/script.js");
-        header("Content-type: /js/scriptArtist.js");
-        header("Content-type: /js/scriptProfil.js");
-        ?>
+        <script src="/js/script.js" defer></script>
+        <script src="/js/scriptArtist.js" defer></script>
+        <script src="/js/scriptProfil.js" defer></script>
         <title>Dao Family Records</title>
     </head>
 
