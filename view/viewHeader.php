0fab11d09b53f9fe60f5b1f73c08d93e2a1fab09
lRaffou <akira182@proton.me>
2024-10-09 19:11:18 +0200

    modif lien script

--- view/viewHeader.php
+++ view/viewHeader.php
@@ -15,9 +15,11 @@ function renderHeader()
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
             integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
             crossorigin="anonymous" referrerpolicy="no-referrer" />
-        <script src="/js/script.js" defer></script>
-        <script src="/js/scriptArtist.js" defer></script>
-        <script src="/js/scriptProfil.js" defer></script>
+        <?php
+        header("Content-type: /js/script.js");
+        header("Content-type: /js/scriptArtist.js");
+        header("Content-type: /js/scriptProfil.js");
+        ?>
         <title>Dao Family Records</title>
     </head>
 
