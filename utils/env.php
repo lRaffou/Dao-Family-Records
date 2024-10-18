eedd4913465815ac8d40a4358a9e55499b182c07
lRaffou <128249344+lRaffou@users.noreply.github.com>
2024-10-18 08:57:36 +0200

    Delete utils/env.php

--- utils/env.php
+++
@@ -1,8 +0,0 @@
-<?php
-//Fichier qui va stocker les variables d'environnement, DONT les données sensibles privées (exemple : identifiant de connexion à une BDD)
-// C'est le premier fichier à mettre dans le .gitignore
-$host = $_ENV['hostBDD'] = 'mysql-dfrecords.alwaysdata.net';
-$dbname = $_ENV['dbnameBDD'] = 'dfrecords_dfr';
-$login = $_ENV['dbLogin'] = 'dfrecords_raff';
-$password = $_ENV['dbPassword'] = '+7q?SEXnqsyC8hakAMDsY.evukj3pr:AbBnT#fg%5Ca=7AHX>ZNPchWTKxECX[Q*';
-?>
