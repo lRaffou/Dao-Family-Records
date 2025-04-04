a416f423e6cb51004a26bec8322a373568cc52b2
lRaffou <akira182@proton.me>
2025-04-04 20:42:52 +0200

    redesign footer

--- view/viewFooter.php
+++ view/viewFooter.php
@@ -3,28 +3,68 @@ function renderFooter()
 {
     ob_start();
     ?>
-    <footer>
-        <section class="containerFooter">
-            <img class="logoFooter" src="/img/1logo_dfr_blanc_complet.png" alt="logo DFR" />
-            <article class="followFooter">
-                <a href="https://www.youtube.com/@DaoFamilyRecords" target="_blank" rel="noopener noreferrer"><img
-                        src="/img/icons8-youtube-logo-250.png" alt="logo youtube" /></a>
-                <a href="https://open.spotify.com/intl-fr/artist/3zpewQ11GYG2ev9Ngdsvi9?si=e9_8b3i3RhqHm2_LtLTInw"
-                    target="_blank" rel="noopener noreferrer"><img src="/img/icons8-spotify-250.png"
-                        alt="logo spotify" /></a>
-                <a href="https://www.instagram.com/dao_family_records/" target="_blank" rel="noopener noreferrer"><img
-                        src="/img/icons8-logo-instagram-250.png" alt="logo instagram" /></a>
-                <a href="https://www.facebook.com/redmarshalofficial" target="_blank" rel="noopener noreferrer"><img
-                        src="/img/icons8-facebook-250.png" alt="logo facebook" /></a>
-                <a href="https://www.linkedin.com/company/dao-family-record/" target="_blank" rel="noopener noreferrer"><img
-                        src="/img/icons8-linkedin-250.png" alt="logo youtube" /></a>
-            </article>
-            <h6>
-                Copyright &copy; - <span id="year"></span> Tous droits
-                réservés Mentions légales
-            </h6>
-        </section>
+    <footer class="mainFooter">
+        <div class="footerContent">
+            <div class="footerSection">
+                <img class="footerLogo" src="/img/1logo_dfr_blanc_complet.png" alt="Dao Family Records" />
+                <p class="footerDescription">Dao Family Records est un label de musique électronique basé à Toulouse,
+                    spécialisé dans la techno et la house.</p>
+            </div>
+
+            <div class="footerSection">
+                <h3>Navigation</h3>
+                <ul class="footerLinks">
+                    <li><a href="/index.php">Accueil</a></li>
+                    <li><a href="/controller/evenement.php">Événements</a></li>
+                    <li><a href="/controller/artiste.php">Artistes</a></li>
+                    <li><a href="/controller/video.php">Vidéos</a></li>
+                    <li><a href="/controller/album.php">Albums</a></li>
+                </ul>
+            </div>
+
+            <div class="footerSection">
+                <h3>Suivez-nous</h3>
+                <div class="socialLinks">
+                    <a href="https://www.youtube.com/@DaoFamilyRecords" target="_blank" rel="noopener noreferrer"
+                        class="socialLink">
+                        <i class="fab fa-youtube"></i>
+                        <span>YouTube</span>
+                    </a>
+                    <a href="https://open.spotify.com/intl-fr/artist/3zpewQ11GYG2ev9Ngdsvi9?si=e9_8b3i3RhqHm2_LtLTInw"
+                        target="_blank" rel="noopener noreferrer" class="socialLink">
+                        <i class="fab fa-spotify"></i>
+                        <span>Spotify</span>
+                    </a>
+                    <a href="https://www.instagram.com/dao_family_records/" target="_blank" rel="noopener noreferrer"
+                        class="socialLink">
+                        <i class="fab fa-instagram"></i>
+                        <span>Instagram</span>
+                    </a>
+                    <a href="https://www.facebook.com/redmarshalofficial" target="_blank" rel="noopener noreferrer"
+                        class="socialLink">
+                        <i class="fab fa-facebook"></i>
+                        <span>Facebook</span>
+                    </a>
+                    <a href="https://www.linkedin.com/company/dao-family-record/" target="_blank" rel="noopener noreferrer"
+                        class="socialLink">
+                        <i class="fab fa-linkedin"></i>
+                        <span>LinkedIn</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="footerBottom">
+            <p class="copyright">
+                Copyright &copy; <span id="year"></span> Dao Family Records. Tous droits réservés.
+                <a href="/mentions-legales" class="legalLink">Mentions légales</a>
+            </p>
+        </div>
     </footer>
+
+    <script>
+        document.getElementById('year').textContent = new Date().getFullYear();
+    </script>
     <?php
     return ob_get_clean();
 }
